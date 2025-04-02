a4ba48e0496d040a407d361d4fb53066bd2659ed
kinderlokkerr <f.meddens97@gmail.com>
2025-04-02 17:34:01 +0200

    player fix

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ let coinColor;
 let enemyColor;
 
 function preload () {
-    PlayerImage= loadImage('/MacintoshHD/users/finnmeddens/player.png');
+    PlayerImage= loadImage('/MacintoshHD/users/finnmeddens/WebstormProjects/untitled/player.png');
 }
 
 
